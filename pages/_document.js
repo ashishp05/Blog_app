bd09c52960daecc49ee822be54294ae16ce3fb06
ashishp05 <hariparaashish6686@gmail.com>
2025-07-09 15:08:41 +0530

    Analytics Added

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { Analytics } from '@vercel/analytics/next';
 export default function Document() {
   return (
     <Html lang="en">
@@ -7,7 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-       
+          <Analytics />
       </body>
     </Html>
   );
